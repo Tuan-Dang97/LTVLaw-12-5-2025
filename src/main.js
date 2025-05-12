b9184b2bc7cb40aa042b3a493d814556065961d8
jonicage6 <tuandq2806@gmail.com>
2025-05-12 22:13:11 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./css/layout/style.css";
+import "./css/pages/about.css";
+import "./css/pages/article.css";
+import "./css/pages/blog.css";
+import "./css/pages/client-stores.css";
+import "./css/pages/recruitment.css"
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+    // Đường dẫn gốc khi deploy (gh-pages cần đúng path)
+    base: "/LTVLaw",
+
+    // Thư mục chính chứa HTML, SCSS, JS
+    root: "src",
+
+    // Thư mục chứa các file tĩnh như ảnh, fonts, v.v.
+    publicDir: "../public",
+
+    build: {
+        // Nơi xuất ra sản phẩm sau khi build
+        outDir: "../dist",
+        emptyOutDir: true,
+
+        // Khai báo các trang cần build (nhiều trang)
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+                article: path.resolve(__dirname, "src/article-template.html"),
+                blog: path.resolve(__dirname, "src/blog.html"),
+                client: path.resolve(__dirname, "src/client-stores.html"),
+                recruitment: path.resolve(__dirname, "src/recruitment.html"),
+                // Thêm trang khác nếu có
+            },
+        },
+    },
+
+    plugins: [ViteEjsPlugin()],
+});
